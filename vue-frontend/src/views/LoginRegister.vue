dd02d761c95009367f463dcc88cef1908cf6b8f5
Rami <rami.albadri77@gmail.com>
2019-04-15 20:30:32 +0200

    fixed login + created register

--- vue-frontend/src/views/LoginRegister.vue
+++ vue-frontend/src/views/LoginRegister.vue
@@ -1,67 +1,39 @@
 <template>
-<div id="app">
-  <v-app id="inspire">
-    <v-form
-      ref="form"
-      v-model="valid"
-      lazy-validation
-    >
-      <v-text-field
-        v-model="name"
-        :counter="10"
-        :rules="nameRules"
-        label="Name"
-        required
-      ></v-text-field>
-  
-      <v-text-field
-        v-model="email"
-        :rules="emailRules"
-        label="E-mail"
-        required
-      ></v-text-field>
-  
-      <v-btn
-        :disabled="!valid"
-        color="success"
-        @click="validate"
-      >
-        Log in
-      </v-btn>
-    </v-form>
-  </v-app>
-</div>
-
+  <div class="form">
+    <Login />
+    <Register />
+  </div>
 </template>
 
 <script>
 
+import Register from "@/components/Register.vue"
+import Login from "@/components/Login.vue"
+
 export default {
-  data: () => ({
-    valid: true,
-    name: '',
-    nameRules: [
-      v => !!v || 'Name is required',
-      v => (v && v.length <= 10) || 'Name must be less than 10 characters'
-    ],
-    email: '',
-    emailRules: [
-      v => !!v || 'E-mail is required',
-      v => /.+@.+/.test(v) || 'E-mail must be valid'
-    ]
-  }),
-
-  methods: {
-    validate () {
-      if (this.$refs.form.validate()) {
-        this.snackbar = true
-      }
-    }
+  components:{
+    Register,
+    Login
   }
-
 }
 </script>
 
 <style scoped>
+.form{
+  overflow: hidden;
+  display: flex;   
+  height: 60vh;
+  margin-left: 6vw;
+}
 
+@media screen and (max-width:667px ){
+  .form{
+    width: 90vw;
+    margin-top: 4vh;
+    margin-left: 5vw;
+    display: inline-block;
+    overflow: scroll;
+    height: 80vh;
+  }
+}
 </style>
