ebe8e256a0ff66c5f72a66972bdcc5e0b6542ad3
Rami <rami.albadri77@gmail.com>
2019-04-16 09:20:33 +0200

    merge

--- vue-frontend/src/components/BottomFooter.vue
+++ vue-frontend/src/components/BottomFooter.vue
@@ -1,12 +1,27 @@
 <template>
-  <v-footer class="pa-3">
-    <v-spacer></v-spacer>
-    <div>&copy; {{ new Date().getFullYear() }}</div>
+  <v-footer id="bottomFooter" class="pa-5" color="white"  >
+    <p>10 Downing Street, London</p>
+    <p>+44 (0)7845 804513</p>
+    <p>&copy;Tree {{ new Date().getFullYear() }}</p>
+    <br>
+    <p>info@tree.com</p>
+
   </v-footer>
 </template>
 
 <script>
 export default {
-    name: "BottomFooter"
-}
+  name: "BottomFooter"
+};
 </script>
+
+<style>
+#bottomFooter {
+  display: flex;
+  justify-content: center;
+  justify-content: space-around;
+  height: 200px;
+
+}
+</style>
+
