1641cb70cd5af9acbfea6f22e314bb5fe524e757
Eric Rasmusson Lindström <eric.rl@me.com>
2019-04-24 11:42:07 +0200

    Updaterar listan efter man lagt in annons

--- vue-frontend/src/views/Add.vue
+++ vue-frontend/src/views/Add.vue
@@ -87,7 +87,7 @@ export default {
     };
   },
   methods: {
-    addAuction() {
+    async addAuction() {
       if (this.date == "") {
         return alert("Please pick an end date");
       } else {
@@ -126,8 +126,8 @@ export default {
         //   auctionID: '1'
         // };
 
-        this.$store.dispatch("addAuctionToDB", productData);
-        this.$router.push("/"); //Går till startsidan
+        await this.$store.dispatch("addAuctionToDB", productData);
+        await this.$router.push("/"); //Går till startsidan
       }
     },
     onPickFile() {
