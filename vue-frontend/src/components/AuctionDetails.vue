a9c69cc64204c45619e2657931c4569503cb6bc2
Ted <ted.agner@hotmail.com>
2019-04-24 12:59:35 +0200

    Added picture to auction from backend to frontend

--- vue-frontend/src/components/AuctionDetails.vue
+++ vue-frontend/src/components/AuctionDetails.vue
@@ -112,15 +112,18 @@ export default {
   },
   async created() {
     await this.$store.dispatch("getOneAuction", this.$route.params.id)
-    this.auction = this.$store.state.currentAuction;    
+    this.auction = this.$store.state.currentAuction;
 
     await this.$store.dispatch("getSeller", this.auction.seller_id)
     this.seller = this.$store.state.currentSeller;    
 
+    this.items =[{ src: await fetch("http://localhost:7999/images/" + this.auction.image).then(res => res.url)
+    }]
+    
     this.getBids();
 
-    this.items = [{src:this.auction.image}]
-    console.log(this.items)
+    // this.items = [{src:this.auction.image}]
+    await console.log(this.items)
   
     this.countdown = new Date(this.auction.end_time).toLocaleString()
     this.forceRerender();
